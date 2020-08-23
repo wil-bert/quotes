3dc1827ccf512c643511a4aa82e5bccd368df3ff
wil-bert <ianwilbuts@gmail.com>
2020-08-23 11:07:08 +0300

    Add detail-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
+    DetailComponent,
 
   ],
   imports: [
