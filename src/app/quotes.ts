9dc847c715df06972107a298d3b3acf3036dfe04
wil-bert <ianwilbuts@gmail.com>
2020-08-23 14:26:30 +0300

    Export date,upvotes,downvotes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -3,7 +3,9 @@ export class Quotes {
     public downvotes: number
     public completeDate: Date
     constructor (public quote:string, public author:string, public person:string){
-
+        this.upvotes = 0;
+        this.downvotes = 0;
+        this.completeDate = new Date;
     }
 
 }
