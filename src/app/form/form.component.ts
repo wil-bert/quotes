9c7bfcfa10ba9d93a3b497385983dc31543d5747
wil-bert <ianwilbuts@gmail.com>
2020-08-23 20:43:33 +0300

    Edit form.ts

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
+
 
 @Component({
   selector: 'app-form',
@@ -6,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  @Output() emitQuote = new EventEmitter()
+  quoteWords:string
+  quoteEditor:string
+  quoteAuthor:string
+  quoteDate:number
+  theWords:any
+
+  submitQuote(){
+    this.theWords = new Quotes(this.quoteEditor,this.quoteAuthor,this.quoteWords)
+    this.quoteWords=''
+    this.quoteAuthor=''
+    this.quoteEditor=''
+    this.emitQuote.emit(this.theWords)
+  }
+
+
 
   constructor() { }
 
